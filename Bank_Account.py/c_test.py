e3063fd78278df486e4449af102debcbb805a8c6
EswaranS-06 <eswaransk06@gmail.com>
2025-06-12 12:35:20 +0530

    Banking CLI is completed

--- Bank_Account.py/c_test.py
+++ Bank_Account.py/c_test.py
@@ -1,10 +1,11 @@
 import time as t
+import error_check as ec
 class Bank():
     
     def __init__(self, account_holder, age):
         
         self.account_holder = account_holder
-        print(f"\n An Account has been created Successfully under the name of {self.account_holder} \n\n")
+        print(f"\n An Account has been created Successfully under the name of [{self.account_holder}] \n\n")
         self.balance = 0.0
         self.age = age
         
@@ -17,16 +18,28 @@ class Bank():
         
     def withdraw(self, withdraws):
         if withdraws > self.balance:
-            raise ValueError(f"\nYour A/C balance is {self.balance} So your unable to withdraw {withdraws}")
+            raise ValueError(f"\nYour A/C balance is [{self.balance}] So your unable to withdraw [{withdraws}]")
             
         self.balance -= withdraws
         print("Withdrawal successful!\n")
         
     def get_balance(self):
-        print(f"\tYour Account Balance \n _________________________ \n {self.balance}$ \n _________________________ \n\n ")
+        print(f"""
+    Your Account Balance
+    _________________________
+        {self.balance}$ 
+    _________________________ 
+    """)
         
     def __str__(self):
-        return f" \tAccount Holder,s Details \n \tName:- {self.account_holder} \n \tBalance:- {self.balance} \n _________________________\n \tAge:- {self.age}  \n \n"
+        return f""" 
+    _________________________
+    Account Holder,s Details
+        - Name:- {self.account_holder}
+        - Age:- {self.age}
+        - Balance:- {self.balance}
+    _________________________
+    """
 cus = {}    
 print('Welcome to CLI Bank ')
 while(True):
@@ -37,8 +50,52 @@ while(True):
     c = int(input('Enter your choie:'))
     if c == 1:
         name = input('Enter your Name:')
-        age = int(input('Enter your Name:'))
-        t.sleep(2)
+        age = int(input('Enter your Age:'))
+        t.sleep(1)
         ac = int(input('Crate a New Account Number 1000-9999: '))
-        t.sleep(4)
-        if ac 
+        t.sleep(1)
+        if ec.none_check(cus, ac):
+            acc = Bank(name, age)
+            cus.update({ac:acc})
+            c = 2
+        else:
+            print(f'''
+        The Account number [{ac}] is Already Taken''')
+            
+    if c == 2:
+        
+        ac = int(input('Enter your A/c no: '))
+        if ec.none_check(cus, ac):
+            print(f'''
+        Sorry, You don't have an Account in this [{ac}]
+        Please create an Account to continue  
+                ''')
+        else:        
+            print("""\tSelect an option: 
+                        1. Deposit
+                        2. Withdraw
+                        3. Check Balance
+                        4. Account Info
+                        0. Exit
+                        """)
+            
+            while True:
+                c = int(input("Enter your choise:- "))
+                op = cus[ac]   
+                if c == 1:
+                    op.deposit(int(input("Enter the amount to Deposit:- ")))
+                    
+                elif c == 2:
+                    op.withdraw(int(input("Enter the amount to Withdraw:- ")))
+                        
+                elif c == 3:
+                    op.get_balance()
+                        
+                elif c == 4:
+                    print(op)
+                    
+                elif c == 0:
+                    break
+                
+    if c == 3:
+        ec.write(cus)
--- Bank_Account.py/error_check.py
+++ Bank_Account.py/error_check.py
@@ -1,3 +1,19 @@
-i = {}
-i.update({1:'er'})
-i.update({1:'et'})
+import json
+def none_check(cus, ac):
+    return cus.get(ac) == None
+def write(cus):
+    with open('info.txt', 'w') as info:
+        info.write(cus)
+        
+# a Python object (dict):
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
