e72932234542905491d1ba8d1b1e648068e74db8
EswaranS-06 <eswaransk06@gmail.com>
2025-06-13 15:24:50 +0530

    the sql db python bank account managing program have been done

---
+++ Bank_Account.py/MySQL_CLI Bank_Account.py
@@ -0,0 +1,62 @@
+import bank_sql as sql
+import random as r
+
+print('Welcome to the CLI Bank')
+while True:
+    print('''
+    1. To Create an Account
+    2. To login into the Existing A/c
+    0. To Exit
+          ''')
+    c = int(input('Enter your choise: '))
+    
+    #if to  create an account
+    if c == 1:
+        name = input('Enter your Name:- ')
+        address = input('Enter your Address:- ')
+        phone = int(input('Enter your Phone No:- '))
+        ac = r.randint(1000,9999)
+        
+        sql.data_insert(name=name, ac=ac, phone=phone, address= address)
+    #if to login to the existing account   
+    if c == 2:
+        ac = int(input('Enter your A/c no: '))
+        print("""\t
+        Select an option: 
+            1. Deposit
+            2. Withdraw
+            3. Transfer Fund
+            4. Check Balance
+            5. Account Info
+            0. Exit
+            """)
+        while (sql.data_sel(ac=ac)):
+            c = int(input('Enter your choise: '))
+            if c == 1:
+                dep = int(input('Enter the amount to Deposit: '))
+                sql.add_bal(ac=ac, dep=dep)
+                
+            
+            elif c == 2:
+                draw = int(input('Enter the amount to Withdraw: '))
+                sql.draw_bal(ac=ac, draw=draw)
+                
+            elif c == 3:
+                tac = int(input("Enter the A/c no to Transfer fund: "))
+                fund = int(input("Enter the amount to Transfer: "))
+                print(sql.transfer(ac=ac, tac=tac, fund=fund))
+            
+            elif c == 4:
+                sql.check_bal(ac=ac)
+            
+            elif c == 5:
+                sql.ac_info(ac=ac)
+            
+            elif c == 0:
+                break
+            
+            else:
+                print('Invalid selction')
+                
+    if c == 999:
+        sql.all_info()
--- Bank_Account.py/bank_sql.py
+++ Bank_Account.py/bank_sql.py
@@ -7,22 +7,160 @@ mydb = sql.connect(
     database='bank'
 )
 
-cmd = mydb.cursor()
-
-def data_insert(ac, name, phone):
-    mydb = sql.connect(
-        host='localhost',
-        user='root',
-        password='2726',
-        database='bank'
-    )
-
-    cmd = mydb.cursor()
-    cm = 'insert into customers (ac_no, name, phone_no) values (%s,%s,%s)'
-    val = (ac, name, phone)
+def all_info():
+    mc = mydb.cursor()
+    cmd = 'Select * from customers '
+    mc.execute(cmd)
+    allInfo = mc.fetchall()
+    for info in allInfo:
+        print(f""" 
+        _________________________
+        Account Holder,s Details
+            - Name:- {info[1]}
+            - A/c no:- {info[0]}
+            - Balance:- {info[4]}
+            - Phone no: - {info[2]}
+            - Address:- {info[3]}
+        _________________________
+        """)
     
-    cmd.execute(cm, val)
+    
+def data_insert(ac, name, phone, address):
+
+    mc = mydb.cursor()
+    
+    cmd = 'Select ac_no from customers where ac_no = %s'
+    val = (ac, )
+    mc.execute(cmd, val)
+    
+    if mc.fetchall() != []:
+        print('Sorry, please Create a new account Thank you')
+        return False
+    
+    cmd = 'insert into customers (ac_no, name, phone_no, address, bal) values (%s,%s,%s,%s,%s)'
+    val = (ac, name, phone, address, 0.0)
+    
+    mc.execute(cmd, val)
     
     mydb.commit()
     
-    print(cmd.rowcount, 'was inserted')
+    print(mc.rowcount, 'was inserted')
+    print('A/c no: ',ac)
+    
+def data_sel(ac):
+    mc = mydb.cursor()
+    cmd = 'Select ac_no from customers where ac_no = %s'
+    val = (ac, )
+    mc.execute(cmd, val)
+    
+    if mc.fetchall() == []:
+        print('Sorry there is no account that number\nplease check the A/c number or\nCreate a new account\nThank you')
+        return False
+    
+    return True
+
+def add_bal(ac, dep):
+    mc = mydb.cursor()
+    cmd = 'Select bal from customers where ac_no = %s'
+    val = (ac,)
+    mc.execute(cmd, val)
+    cu_bal = mc.fetchall()[0][0]
+    bal = cu_bal + dep
+    cmd = 'update customers set bal = %s where ac_no = %s' 
+    val = (bal, ac)
+    
+    mc.execute(cmd, val)
+    
+    mydb.commit()
+    print("The Amount has been Credited\n_________________________________\n")
+    
+def draw_bal(ac, draw):
+    mc = mydb.cursor()
+    cmd = 'Select bal from customers where ac_no = %s'
+    val = (ac,)
+    mc.execute(cmd, val)
+    cu_bal = mc.fetchall()[0][0]
+    
+    if cu_bal <= draw:
+        print("Insufficient Balance")
+        return
+    
+    bal = cu_bal - draw
+    cmd = 'update customers set bal = %s where ac_no = %s' 
+    val = (bal, ac)
+    
+    mc.execute(cmd, val)
+    
+    mydb.commit()
+    print("The Amount has been Debited\n_________________________________\n")
+    
+def check_bal(ac):
+    mc = mydb.cursor()
+    cmd = 'Select bal from customers where ac_no = %s'
+    val = (ac,)
+    mc.execute(cmd, val)
+    cu_bal = mc.fetchall()[0][0]
+    print(f"""
+    Your Account Balance
+    _________________________
+        {cu_bal}₹ 
+    _________________________ 
+    """)
+    
+def ac_info(ac):
+    mc = mydb.cursor()
+    cmd = 'Select * from customers where ac_no = %s'
+    val = (ac,)
+    mc.execute(cmd, val)
+    info = mc.fetchone()
+    print(f""" 
+    _________________________
+    Account Holder,s Details
+        - Name:- {info[1]}
+        - A/c no:- {info[0]}
+        - Balance:- {info[4]} ₹
+        - Phone no: - {info[2]}
+        - Address:- {info[3]}
+    _________________________
+    """)
+    
+def transfer(ac, tac, fund):
+    mc = mydb.cursor()
+    cmd = 'Select ac_no from customers where ac_no = %s'
+    val = (tac, )
+    mc.execute(cmd, val)
+    
+    if mc.fetchall() == []:
+        return "Sorry Invalid A/c no to Transfer\n_________________________________\n"
+    
+    #get balance form login user ac
+    cmd = 'select bal from customers where ac_no = %s'
+    val = (ac,)
+    mc.execute(cmd, val)
+    cu_bal = mc.fetchone()[0]
+    
+    if cu_bal < fund:
+        return f'Insufficient Balance current balance = {cu_bal}\n_________________________________\n'
+    
+    #get balance form transfer user ac
+    val = (tac,)
+    mc.execute(cmd, val)
+    tcu_bal = mc.fetchone()[0]
+    
+    #opreation to tranfer fund
+    bal = cu_bal - fund
+    
+    tbal = tcu_bal + fund
+    
+    cmd = 'update customers set bal = %s where ac_no = %s'
+    #set balance into the login user ac
+    val = (bal, ac)
+    mc.execute(cmd, val)
+    mydb.commit()
+    
+    #set balance into the transfer user ac
+    val = (tbal, tac)
+    mc.execute(cmd, val)
+    mydb.commit()
+    
+    return f'The Fund[{fund} ₹] as been successfully Transfered to the A/c no [{tac}]\n_________________________________\n'
